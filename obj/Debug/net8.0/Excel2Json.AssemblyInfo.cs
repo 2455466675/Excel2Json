583c57a2e79246e6023311a74c786a7f34b4591c
lichuang <lichuang@xcarry.com>
2025-01-13 10:52:14 +0800

    删掉bin

--- obj/Debug/net8.0/Excel2Json.AssemblyInfo.cs
+++ obj/Debug/net8.0/Excel2Json.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Excel2Json")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19cfe07b9e532438c6699570edbf88629a3dc858")]
 [assembly: System.Reflection.AssemblyProductAttribute("Excel2Json")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Excel2Json")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
